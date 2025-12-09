882d155fe2c059b0e8a3476b88234e0abc40c69d
Brad-Munson <brmu9866@colorado.edu>
2025-12-08 18:55:52 -0700

    updated 14

---
+++ week14/week14.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("menuToggle");
+const menu = document.getElementById("navMenu");
+
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("open");
+});
